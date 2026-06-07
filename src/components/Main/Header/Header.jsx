d348a84761df8e573588f1a01d3311050c17c6d4
kolya-makarenko <lifestealer27@gmail.com>
2026-06-07 03:18:39 +0300

    media screen start on main page

--- src/components/Main/Header/Header.jsx
+++ src/components/Main/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import classes from './Header.module.css';
 
@@ -14,12 +15,17 @@ const MENU_ITEMS = [
 ];
 
 const Header = () => {
+    const [isMenuActive, setIsMenuActive] = useState(false);
     const navigate = useNavigate();
 
     const getNavLinkClass = ({ isActive }) => {
         return isActive
             ? `${classes.mainMenuLink} ${classes.active}`
             : classes.mainMenuLink;
+    };
+
+    const openMobileMenu = () => {
+        setIsMenuActive(!isMenuActive);
     };
 
     return (
@@ -30,20 +36,48 @@ const Header = () => {
                         <NavLink to="/" className={classes.logoMain}>
                             <img src={logo} alt="logo" />
                         </NavLink>
-                        <nav className={classes.mainNavigation}>
+                        <nav
+                            className={
+                                isMenuActive
+                                    ? `${classes.mainNavigation} ${classes.active}`
+                                    : classes.mainNavigation
+                            }
+                        >
                             <ul>
                                 {MENU_ITEMS.map((item, index) => (
                                     <li key={index}>
                                         <NavLink
                                             to={item.to}
                                             className={getNavLinkClass}
+                                            onClick={openMobileMenu}
                                         >
                                             {item.label}
                                         </NavLink>
                                     </li>
                                 ))}
+                                <li className={classes.toContactsLink}>
+                                    <NavLink
+                                        to="/contact-us"
+                                        className={getNavLinkClass}
+                                        onClick={openMobileMenu}
+                                    >
+                                        Contact Us
+                                    </NavLink>
+                                </li>
                             </ul>
                         </nav>
+                        <div
+                            className={
+                                isMenuActive
+                                    ? `${classes.burger} ${classes.active}`
+                                    : classes.burger
+                            }
+                            onClick={openMobileMenu}
+                        >
+                            <div className={classes.burgerLine1}></div>
+                            <div className={classes.burgerLine2}></div>
+                            <div className={classes.burgerLine3}></div>
+                        </div>
                     </div>
                     <button
                         className={classes.btnToContacts}
